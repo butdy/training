75fcd5be4c77048fcd0980ca913c8e6993b470f8
butdy <butom.gogo@gmail.com>
2021-03-11 11:38:15 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,2 @@
-echo "<h1>Test index CICD versi 1</h1>" > index.html
-echo "<h1>Test index CICD versi keDua</h1>" > index.html
+echo "<h1>Test index CICD versi 1</h1>" >> index.html
+echo "<h1>Test index CICD versi keDua</h1>" >> index.html
